3313d446f03d4c3c8e573635ee5abf7f2965056c
Laurent Fourcadier <ouhman@gmail.com>
2026-01-10 22:09:55 +0100

    chore: update local settings and styling

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
     <RouterView />
+    <ToastContainer />
   </div>
 </template>
 
 <script setup lang="ts">
 import { RouterView } from 'vue-router'
+import ToastContainer from '@/components/common/ToastContainer.vue'
 </script>
 
 <style>
