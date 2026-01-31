5286a93f767599b935250cdfd87bbb989444d990
Laurent Fourcadier <ouhman@gmail.com>
2026-01-31 10:44:43 +0100

    feat(types): add centralized type exports barrel file
    Allows convenient imports: import { Location, Category } from '@/types'

---
+++ src/types/index.ts
@@ -0,0 +1,14 @@
+/**
+ * Centralized type exports
+ *
+ * Re-exports all types from individual type files for convenient imports.
+ * Usage: import { Location, Category, OsmFeature } from '@/types'
+ */
+
+export * from './database'
+export * from './osm'
+export * from './hours'
+export * from './marker'
+export * from './analytics'
+export * from './consent'
+export * from './enrichment'
